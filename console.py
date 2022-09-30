689ea264b10a3a40b33a0b5edc555ee24bfbab43
root <nick.french0307@gmail.com>
2022-09-30 10:59:30 -0700

    console

--- console.py
+++ console.py
@@ -9,23 +9,22 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-	"""Creates class Geometry"""
+    """Creates class Gemoetry"""
 
-		def __init__(self, width, height):
-			"""Initializes Rectangle class"""
+    def __init__(self, width, height):
+        """Initializes Rectangle class"""
 
-			self.integer_validator("width", width)
-			self.integer_validator("height", height)
-			self.__width = width
-			self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
 
-		def area(self):
-			"""Return the area of the rectangle."""
+    def area(self):
+        """Return the area of the rectangle."""
 
-			return self.__width * self.__height
+        return self.__width * self.__height
 
-		def __str__(self):
-			"""Returns a formatted string."""
+    def __str__(self):
+        """Returns a formatted string."""
 
-			return str("[Rectangle] {}/{}".format(self.__width, self.__height))
-
+        return str("[Rectangle] {}/{}".format(self.__width, self.__height))
