fd35cd3ca2d5bb6bb19030f78b8f0098f9ba12e7
EthanWakeford <ethanwakeford@gmail.com>
2022-09-30 12:21:11 -0500

    changed type in __str__ method

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 from uuid import uuid4
 from datetime import datetime
 
+
 class BaseModel:
     """basemodel class for other classes"""
 
@@ -12,7 +13,7 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def __str__(self):
-        return ("[{}] ({}) {}".format(self.__class__, self.id, self.__dict__))
+        return (f"[{type(self).__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
         """updates the self.updated at attribute"""
@@ -24,4 +25,3 @@ class BaseModel:
         self.created_at = self.created_at.isoformat()
         self.updated_at = self.updated_at.isoformat()
         return self.__dict__
-
