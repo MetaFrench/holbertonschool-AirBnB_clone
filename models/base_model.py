148a34769f8c8927af55893c2ff91123c9d80ad2
EthanWakeford <ethanwakeford@gmail.com>
2022-09-30 15:56:29 -0500

    did everythin

--- models/base_model.py
+++ models/base_model.py
@@ -2,34 +2,40 @@
 """module holds a class BaseModel"""
 from uuid import uuid4
 from datetime import datetime
+import models
 
 
 class BaseModel:
     """basemodel class for other classes"""
 
-    def __init__(self, *args, **kwargs):    
+    def __init__(self, *args, **kwargs):
         if kwargs:
             for key, value in kwargs.items():
                 if key == '__class__':
                     continue
-                    if key == 'updated_at' or key == 'created_at':
-                        key.strptime()
+                if key == 'updated_at' or key == 'created_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 setattr(self, key, value)
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-    
+            models.storage.new(self)
+
     def __str__(self):
         return (f"[{type(self).__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
         """updates the self.updated at attribute"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """returns a dict containing the instance attributes"""
-        self.__dict__['__class__'] = type(self).__name__
-        self.created_at = self.created_at.isoformat()
-        self.updated_at = self.updated_at.isoformat()
-        return self.__dict__
+        my_dict = self.__dict__.copy()
+        my_dict['__class__'] = type(self).__name__
+        if type(my_dict['created_at']) is not str:
+            my_dict['created_at'] = self.created_at.isoformat()
+        if type(my_dict['updated_at']) is not str:
+            my_dict['updated_at'] = self.updated_at.isoformat()
+        return my_dict
---
+++ models/engine/file_storage.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""has a class FileStorage that sFileStorage"""
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """FileStorage"""
+
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self):
+        """returns __objects dict"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """adds obj to __objects dict"""
+        FileStorage.__objects[f'{type(obj).__name__}.{obj.id}'] = obj
+
+    def save(self):
+        """serializes __objects dict to json file"""
+        my_dict = {}
+        with open(FileStorage.__file_path, 'w', encoding='utf-8') as f:
+            for key, value in FileStorage.__objects.items():
+                my_dict[key] = value.to_dict()
+            f.write(json.dumps(my_dict))
+
+    def reload(self):
+        """deserializes the json file into __objects dict"""
+        try:
+            with open(FileStorage.__file_path, encoding='utf-8') as f:
+                all_objs = (json.loads(f.read()))
+                for obj_id in all_objs.keys():
+                    obj = all_objs[obj_id]
+                    cls_name = obj['__class__']
+                    self.new(eval(cls_name)(**obj))
+        except Exception:
+            pass
