5d24d753323c0748966578d5d55444916ed8d533
EthanWakeford <ethanwakeford@gmail.com>
2022-09-30 13:03:28 -0500

    baseModel init can take args and kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,19 @@ from datetime import datetime
 class BaseModel:
     """basemodel class for other classes"""
 
-    def __init__(self, *args, **kwargs):
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
+    def __init__(self, *args, **kwargs):    
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                    if key == 'updated_at' or key == 'created_at':
+                        key.strptime()
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+    
     def __str__(self):
         return (f"[{type(self).__name__}] ({self.id}) {self.__dict__}")
 
