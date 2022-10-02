74cbf6a06231c4c7061535450f39ebdfedf996ed
EthanWakeford <ethanwakeford@gmail.com>
2022-10-02 17:08:23 -0500

    added documentation and changed imports

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+"""initializes models module"""
+from models.engine.file_storage import FileStorage
+from models import base_model
+from models.base_model import BaseModel
+
+storage = FileStorage()
+storage.reload()
