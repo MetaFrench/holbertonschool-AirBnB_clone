6f76ac997754252b87a7d34364d0b5e1bb328a4a
EthanWakeford <ethanwakeford@gmail.com>
2022-10-02 17:29:31 -0500

    added city class

---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Has a class City"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """class that defines a city"""
+
+    state_id = ""
+    name = ""
